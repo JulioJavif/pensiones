71873af3ea853a93ca7e23d8de981d4df364ef4e
Julio Fuentes <juliojavif@gmail.com>
2021-08-09 12:31:55 -0500

    Envía correo - No redirecciona

--- control/accion/act_sendMensaje.php
+++ control/accion/act_sendMensaje.php
@@ -12,16 +12,57 @@ if (isset($_POST['asunto']) && $_POST['asunto']!=''
         require_once (__DIR__."/../../modelo/dao/UsuarioDAO.php");
         $destinatario = UsuarioDAO::getUsuarioByIdPension($_GET['ref']);
 
-        
-        //use PHPMailer\PHPMailer\PHPMailer;
-        //use PHPMailer\PHPMailer\Exception;
-
-
-
-        $mail = new PHPMailer(true);
+        include '../../PHPMailer-6.5.0/src/PHPMailer.php';
+        include '../../PHPMailer-6.5.0/src/SMTP.php';
+        //include '../../PHPMailer-6.5.0/src/OAuth.php';
+        include '../../PHPMailer-6.5.0/src/Exception.php';
+        //include '../../PHPMailer-6.5.0/src/POP3.php';
 
         try {
+
+            $emailTo = $destinatario['email'];
+            $subject = "Pensiones: ".$destinatario['nombre']." quiere comunicarse contigo";
+            $bodymail = "<p>".$mensaje."</p><br>"."<p>Para hablar con "
+            .$destinatario['nombre']." puedes escribir a su correo: "
+            .$destinatario['email']." o llamarlo a su número de telefono "
+            .$destinatario['telefono'].".</p>";
+
+            require_once (__DIR__."/../../conf/correo.php");
+            $fromname = "Pensiones app";
+            $host = "smtp.gmail.com";
+            $port = "587";
+            $SMTPAuth = 'login';
+            $_SMTPSecure = 'tls';
+            $correo = new Correo();
             
+            $mail = new PHPMailer(true);//PHPMailer\PHPMailer\
+
+            $mail->isSMTP();
+
+            $mail->SMTPDebug = 1;
+            $mail->Host = $host;
+            $mail->Port = $port;
+            $mail->SMTPAuth = $SMTPAuth;
+            $mail->SMTPSecure = $_SMTPSecure;
+            $mail->Username = $correo->getCorreo();
+            $mail->Password = $correo->getContrasena();
+
+            $mail->setFrom($correo->getCorreo(), $fromname);
+            $mail->addAddress($emailTo);
+
+            $mail->isHTML(true);
+            $mail->Subject = $subject;
+
+            $mail->Body = $bodymail;
+
+            //var_dump("123456");
+
+            if (!$mail->send()) {
+                echo '\nNo se envió';
+            }else {
+                echo '\nCorreo enviado';
+            }
+            //echo '\nPasa algo raro';
         } catch (Exception $e) {
             //throw $th;
         }
